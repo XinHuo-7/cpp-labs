852a7fca4798b439f25e81836554ad95bad2883e
XinHuo-7 <kynxh12@gmail.com>
2026-08-30 20:50:29 +0300

    w5d2 update ctest of mutex

---
+++ labs/week05_concurrent_event_service/tests/event_queue_test.cpp
@@ -0,0 +1,67 @@
+#include "event_queue.h"
+
+#include <cassert>
+#include <string>
+#include <thread>
+
+void TestEmptyQueue() {
+    EventQueue queue;
+    PortEvent event;
+
+    assert(queue.Size() == 0);
+    assert(!queue.TryPop(event));
+}
+
+void TestPushAndPopInOrder() {
+    EventQueue queue;
+
+    queue.Push({"Ethernet0", "LINK_UP"});
+    queue.Push({"Ethernet4", "LINK_DOWN"});
+
+    PortEvent event;
+
+    assert(queue.TryPop(event));
+    assert(event.portName == "Ethernet0");
+    assert(event.description == "LINK_UP");
+
+    assert(queue.TryPop(event));
+    assert(event.portName == "Ethernet4");
+    assert(event.description == "LINK_DOWN");
+
+    assert(!queue.TryPop(event));
+    assert(queue.Size() == 0);
+}
+
+void Produce(EventQueue& queue, const std::string& produceName) {
+    for (int index = 0; index < 100; ++index) {
+        queue.Push({produceName + "-Ethernet" + std::to_string(index),
+        "LINK_CHANGED"});
+    }
+}
+
+void TestConcurrentPush() {
+    EventQueue queue;
+
+    std::thread producerA(Produce, std::ref(queue), "A");
+    std::thread producerB(Produce, std::ref(queue), "B");
+    
+    producerA.join();
+    producerB.join();
+
+    assert(queue.Size() == 200);
+
+    PortEvent event;
+    int poppedCount = 0;
+    while (queue.TryPop(event)) {
+        ++poppedCount;
+    }
+    assert(poppedCount == 200);
+    assert(queue.Size() == 0);
+
+}
+
+int main() {
+    TestEmptyQueue();
+    TestPushAndPopInOrder();
+    TestConcurrentPush();
+}
